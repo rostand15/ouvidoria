b8b63a8e12fa3ac740a6171a7fcc54dc9f9094f6
Rostand Araújo <rostandaraujo3@gmail.com>
2025-04-03 15:52:37 -0300

    removendo aquirvo quest 4

--- quest4.py
+++
@@ -1,4 +0,0 @@
-preço1 = float(input("digite o primeiro preço "))
-preço2 = float(input("digite o segundo preço "))
-preço3 = float(input("digite o terceiro preço "))
-print(f"a media aritmetica será {(preço1+preço2+preço3)/3:.2f}")
